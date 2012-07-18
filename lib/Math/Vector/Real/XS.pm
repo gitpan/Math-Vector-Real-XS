165800329450b8029eb1da614748e12c255b9e01
Salvador Fandiño García <salva@cpan.org>
2012-07-18 08:12:09 +0000

    Import of SALVA/Math-Vector-Real-XS-0.02 from CPAN.
    gitpan-cpan-distribution: Math-Vector-Real-XS
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SALVA/Math-Vector-Real-XS-0.02.tar.gz
    gitpan-cpan-author:       SALVA
    gitpan-cpan-maturity:     released

--- lib/Math/Vector/Real/XS.pm
+++ lib/Math/Vector/Real/XS.pm
@@ -1,6 +1,6 @@
 package Math::Vector::Real::XS;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use strict;
 use warnings;
@@ -28,6 +28,20 @@ L<Math::Vector::Real> in XS for a great performance boost.
 
 Once this module is installed, L<Math::Vector::Real> will load and use
 it automatically.
+
+=head1 SUPPORT
+
+In order to report bugs you can send me and email to the address that
+appears below or use the CPAN RT bugtracking system available at
+L<http://rt.cpan.org>.
+
+The source for the development version of the module is hosted at
+GitHub: L<https://github.com/salva/p5-Math-Vector-Real-XS>.
+
+=head2 My wishlist
+
+If you like this module and you're feeling generous, take a look at my
+Amazon Wish List: L<http://amzn.com/w/1WU1P6IR5QZ42>
 
 =head1 COPYRIGHT AND LICENSE
 
